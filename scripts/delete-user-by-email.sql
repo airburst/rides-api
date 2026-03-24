70b7f9bd7105e5404e757d6c63db956d9fc83113
Mark Fairhurst <mark@fairhursts.net>
2026-03-24 15:37:20 +0000

    chore: Update script

--- scripts/delete-user-by-email.sql
+++ scripts/delete-user-by-email.sql
@@ -3,9 +3,22 @@
 
 DO $$
 DECLARE
-  _email text := 'user@example.com';  -- <-- change this
+  _email text := 'me@gmail.com'; -- Change this
 BEGIN
-  DELETE FROM bcc_accounts WHERE "userId" IN (SELECT id FROM bcc_users WHERE email = _email);
-  DELETE FROM bcc_sessions WHERE "userId" IN (SELECT id FROM bcc_users WHERE email = _email);
-  DELETE FROM bcc_users WHERE email = _email;
+  DELETE FROM bcc_sessions
+	WHERE user_id IN (
+			SELECT id
+			FROM bcc_users
+			WHERE email = _email
+	);
+
+	DELETE FROM bcc_accounts
+	WHERE user_id IN (
+			SELECT id
+			FROM bcc_users
+			WHERE email = _email
+	);
+
+	DELETE FROM bcc_users WHERE email = _email;
 END $$;
+
