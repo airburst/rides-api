558df331e4bfb2809768dfd0e9bc9500d0e95e3b
Mark Fairhurst <mark@fairhursts.net>
2026-05-13 17:58:38 +0100

    Add RLS security to Supabase

---
+++ drizzle/0014_enable_rls.sql
@@ -0,0 +1,14 @@
+ALTER TABLE "public"."accounts" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."archived_rides" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."archived_users_on_rides" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."club_api_keys" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."clubs" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."memberships" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."repeating_rides" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."rides" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."routes" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."sessions" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."user_clubs" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."users" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."users_on_rides" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."verification_tokens" ENABLE ROW LEVEL SECURITY;
